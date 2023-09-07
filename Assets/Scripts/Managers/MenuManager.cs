7200b132836e47c8433467e69215c6fd68dc0536
hjclark462 <hjclark462@gmail.com>
2023-09-07 11:08:42 +1000

    Update MenuManager.cs

--- Assets/Scripts/Managers/MenuManager.cs
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using TMPro;
 
 public class MenuManager : MonoBehaviour
 {
+    public GameObject m_Title;
+    public GameObject m_MainMenu;
+    public GameObject m_Pause;
+    public GameObject m_Options;
+    public GameObject m_HUD;
+
     // Start is called before the first frame update
     void Start()
     {
