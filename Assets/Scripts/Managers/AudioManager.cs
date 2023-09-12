a956d58cdfb2fdab52b5416c178574fcb905dbed
hjclark462 <81009898+hjclark462@users.noreply.github.com>
2023-09-12 22:53:31 +1000

    Update AudioManager.cs

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -22,45 +22,81 @@ public class AudioManager : MonoBehaviour
     [Header("SFX")]
     public List<AudioClip> m_soundEffects;
 
-    //Source on other things? dragged into this?
-    public AudioSource m_source;
+    //Source on other things? dragged into this? 
+    //Source is object in world so put them where you want them in 3D space attached to a GameObject. Drag them into place in the inspector.
+    //For this set of the variable it should be an array of 2 AudioSources. I kept them playerSources as a suggestion that as this is dialogue
+    //from the player and the items in the players hands the sources can go onto the player. Will develop it more after alpha. Any more is currently
+    //vetoed until further notice as it is not important to the alpha loop.
+    public AudioSource[] m_playerSources;
+    public double m_initDelayLines;
+    int m_playerSourceToggle;
+    double m_nextStartTime;
 
     #region Singleton
-    public static AudioManager instance;
+    private static AudioManager m_instance;
 
-    private void Awake()
+    public static AudioManager Instance
     {
-        if (instance == null)
+        get
         {
-            instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
+            m_instance = FindObjectOfType<AudioManager>();
+            if (m_instance == null)
+            {
+                Debug.LogError("Player Manager is Null!!");
+            }
+            return m_instance;
+
         }
     }
     #endregion
 
-    // Start is called before the first frame update
-    void Start()
+
+    private void Update()
     {
-        
+        if (AudioSettings.dspTime > m_nextStartTime - 1 && m_initialLinesIndex != m_playerSources.Length)
+        {
+            AudioClip clipToPlay = m_voiceLinesInitial[m_initialLinesIndex];
+            // Loads the next Clip to play and schedules when it will start
+            m_playerSources[m_playerSourceToggle].clip = clipToPlay;
+            m_playerSources[m_playerSourceToggle].PlayScheduled(m_nextStartTime);
+            // Checks how long the Clip will last and updates the Next Start Time with a new value
+            double duration = (double)clipToPlay.samples / clipToPlay.frequency;
+            m_nextStartTime = m_nextStartTime + duration;
+            // Switches the toggle to use the other Audio Source next
+            m_playerSourceToggle = 1 - m_playerSourceToggle;
+            // Increase the clip index number, reset if it runs out of clips
+            m_initialLinesIndex++;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    /* Depending on how/when you want this called I see it as to potential options:
+     * 1. You can make a small script and attach it to an empty game object with a collider that calls:
+     *     void OnTriggerEnter(Collider other)
+            {
+                if(other.gameObject.GetComponentInParent<PlayerController>() != null) 
+                  {        
+                    AudioManager.Instance.PlayInitialVoiceLines();
+                  }
+            }
+     * 2. You can tell me when to trigger it if it's off something like the cinematic trigger etc.    
+     */
     public void PlayInitialVoiceLines()
     {
-        m_source.clip = m_voiceLinesInitial[m_initialLinesIndex];
-        m_source.Play();
-        m_initialLinesIndex++;
+        if (m_initialLinesIndex == 0)
+        {
+            AudioClip clipToPlay = m_voiceLinesInitial[m_initialLinesIndex];
+            m_playerSources[m_playerSourceToggle].clip = clipToPlay;
+            m_playerSources[m_playerSourceToggle].PlayScheduled(AudioSettings.dspTime + m_initDelayLines);
+            m_playerSourceToggle = 1 - m_playerSourceToggle;
+            m_initialLinesIndex++;
+            double duration = (double)clipToPlay.samples / clipToPlay.frequency;
+            m_nextStartTime = (m_nextStartTime + duration);
+        }
     }
 
+    /*
+
+    I kept these for reference later. 
     public void PlayCirclePuzzleClip()
     {
         m_source.clip = m_voiceLinesInitial[m_circlePuzzleIndex];
@@ -83,13 +119,14 @@ public class AudioManager : MonoBehaviour
     {
         m_source.clip = m_music[m_musicIndex];
         m_source.Play();
-    }
+    }*/
 
-    // From other script... maybe? bad code, just a thought process.
+    // From other script... maybe? bad code, just a thought process. 
+    // No idea what you had planned here.
     /*public void OnTriggerEnter(Collider other)
     {
         AudioManager.m_musicIndex = 1;
         gameObject.SetActive(false);
     }*/
-    
+
 }
