c3766a926533944b9dd6ce88078bf76c52d678e0
hjclark462 <81009898+hjclark462@users.noreply.github.com>
2023-09-24 19:20:36 +1000

    FMOD

---
+++ Assets/Scripts/BridgeTrigger.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeTrigger : MonoBehaviour
+{
+    public Bridge[] m_sideBridges;
+    public Bridge m_entryBridge;
+
+    private void Start()
+    {
+        foreach(Bridge bridge in m_sideBridges)
+        {
+            bridge.SetBridgeState(false);
+        }
+        m_entryBridge.SetBridgeState(true);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.GetComponentInParent<PlayerController>() != null)
+        {
+            foreach (Bridge bridge in m_sideBridges)
+            {
+                bridge.SetBridgeState(true);
+            }
+            m_entryBridge.SetBridgeState(false);
+            Destroy(this.gameObject);
+        }
+    }
+}
